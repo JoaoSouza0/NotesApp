5318f53459ebf298cb7d83041c9f25844c629c05
João Victor Costa Souza <joao.souza@avenuecode.com>
2021-04-16 14:18:23 -0300

    criação de forms especificos para as operações

---
+++ components/formAddCollection.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h5>Add new collection</h5>
+    <form action="">
+      <label for="nameCollection">Name:</label>
+      <input type="text" id="nameCollection" v-model="name" />
+      <button type="submit" @click.prevent="postNewCollection">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  //
+  name: "formAddCollection",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  computed: {
+    ...mapGetters({ id: "collection/qtCollections" }),
+  },
+  methods: {
+    ...mapActions("collection", { postCollection: "postCollection" }),
+    async postNewCollection() {
+      this.postCollection({ id: this.id, name: this.name });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ components/formEditCollection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h5>Edit collection {{ id }}</h5>
+    <form action="">
+      <label for="nameCollection">Name:</label>
+      <input type="text" id="nameCollection" v-model="name" />
+      <button type="submit" @click.prevent="editCollection">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "formEditCollection",
+  data() {
+    return {
+      name: "",
+    };
+  },
+  props: ["id"],
+  methods: {
+    ...mapActions("collection", { putCollection: "putCollection" }),
+    async editCollection() {
+      this.putCollection({ id: this.id, name: this.name });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
