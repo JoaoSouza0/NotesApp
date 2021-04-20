0c1504d51eb4215792d6ea8500622e2912d24b2d
João Victor Costa Souza <joao.souza@avenuecode.com>
2021-04-20 13:04:14 -0300

    test addCollection

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,16 +1,37 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
 import appAddCollection from '@/components/appAddCollection.vue'
+import { toggleModal } from "@/store/collection/index.js"
 import Vuex from 'vuex'
-import { create } from 'core-js/core/object'
+
 
 describe('Open Modal', () => {
 
     const localVue = createLocalVue()
     localVue.use(Vuex)
 
-    let action, store
-    const wrapper = shallowMount(appAddCollection)
-    test('checando se tem o um botão', () => {
-        expect(wrapper.html()).toContain('h4')
+    let actions, store
+
+    beforeEach(() => {
+        actions = {
+            "toggleModal": jest.fn().mockImplementation(() => {
+            }),
+        };
+        store = new Vuex.Store({
+            actions
+        });
+    });
+    it('open mondal on click', async () => {
+
+        const wrapper = shallowMount(appAddCollection, {
+            localVue,
+            store,
+        })
+
+        //const button = wrapper.find('button')
+        await wrapper.trigger('click')
+
+        expect(actions["toggleModal"])
     })
+
 })
+
