6122d649b92a9af0f347cb5ebf138206cc884232
João Victor Costa Souza <joao.souza@avenuecode.com>
2021-04-23 13:59:17 -0300

    Update head application

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,17 @@
 <script>
 import appHeader from "@/components/appHeader.vue";
 export default {
+  head: {
+    titleTemplate: "Collection App - Nuxt.js",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      { hid: "description", name: "description", content: "Meta description" },
+    ],
+  },
+
   components: {
     appHeader,
   },
@@ -21,13 +32,14 @@ body,
 h1,
 h2,
 ul,
-li ,p,hr{
+li,
+p,
+hr {
   padding: 0;
   margin: 0;
 }
-body{
-    background: #f6f6f6
-;
+body {
+  background: #f6f6f6;
 }
 
 * {
